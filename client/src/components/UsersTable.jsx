b1c326aa0a2e40a59f961009bb84d9e13ef73ae2
jeronimogarcia <jero_garcia23@hotmail.com>
2022-12-08 03:06:22 +0100

    Pagination working. No CSS

--- client/src/components/UsersTable.jsx
+++ client/src/components/UsersTable.jsx
@@ -4,33 +4,40 @@ import Pagination from './Pagination'
 
 const UsersTable = () => {
 
+
+
   const [usersList, setUsersList] = useState([])
   const [numberOfPages, setNumberOfPages] = useState([])
   const [currentPage, setCurrentPage] = useState(1)
+
+  // TODO Check URL PARAM NUMBER WITH CURRENTPAGE (STATE)
+  // TODO Check location.pathname // location.pathname.split("/")
 
   const handlePrevPage = () => {
     if (currentPage > 1){
       setCurrentPage(prevState => prevState - 1)
     }
   }
+  
   const handleNextPage= () => {
     if (currentPage < numberOfPages.length) {
       setCurrentPage(prevState => prevState + 1)
     }
   }
+
   const handleOnClickPage= (number) => {
     setCurrentPage(number)
   }
 
-
   useEffect(() => {
-    fetch('/api')
+    fetch(`/api/pagination/${currentPage}`)
       .then(res => res.json())
       .then(data => {
-        setUsersList(data.requestObject.users)
-        setNumberOfPages(data.requestObject.size)
+        setUsersList(data.responseObject.users)
+        setNumberOfPages(data.responseObject.size)
+        window.history.replaceState(null, null, `/api/pagination/${currentPage}`)
       })
-  }, [])
+  }, [currentPage])
 
   return (
     <div className='mr-8 ml-8 mt-8 pb-8 flex flex-col items-center'>
--- server/server.js
+++ server/server.js
@@ -3,19 +3,40 @@ const http = require("http");
 
 const app = express();
 
+// Importamos el array del archivo JSON
 const usersList = require('./data/users.json')
+
+// Maximo de users
+const maxUsers = 10
+
+// Numero maximo de paginas
 const pages = []
-for( let i = 1; i<= Math.ceil(usersList.length/10); i++){
+for( let i = 1; i<= Math.ceil(usersList.length/maxUsers); i++){
     pages.push(i)
 }
 
-const requestObject = {
+// Creamos el objeto de respueta con el array y el numero maximo de paginas
+const responseObject = {
     users: usersList,
     size: pages
 }
 
+
+
 http.createServer(app).listen(5000);
 
-app.get("/api", (req, res) => {
-    res.json({ requestObject })
+app.get("/api/pagination/:number/:name?", (req, res) => {
+    // Guardamos el numero de pagina que viene por params :number
+    const page = req.params.number
+    // Multiplicador de pagina * 10. Ejemplo, pagina 2 = 20
+    const multiplier = page * maxUsers
+    // Index final donde cortar el array
+    const end = multiplier
+    // Index inicial de donde arranca el array
+    const start = multiplier - maxUsers 
+    // Cortamos el array para mostrar 10 usuarios dependiendo la pagina
+    const newObject = usersList.slice(start, end)
+    // Modificamos el objeto de respueta con el array cortado
+    responseObject.users = newObject
+    res.json({ responseObject })
 })
